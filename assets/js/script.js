8e937f864871c67a2006d4141b760bd724c85fc7
Mahdi Gholami <99770517+MhdiGholami@users.noreply.github.com>
2024-12-06 12:11:02 +0330

    Developed by Mahdi Gholami

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+﻿// Input MaxLength Project
+// **************************
+
+
+// Dom Elements Selection
+let inputElem = document.getElementById('inputId')
+let spanElem = document.querySelector('.counter')
+
+
+// Function for Input keydown Event
+function inpHandler(event) {
+  if (event.key === 'Backspace') {
+    if (spanElem.innerText < 19) {
+      spanElem.innerText = +spanElem.innerText + 1
+    }
+  } else if (event.key === 'Enter' || event.key === 'Shift' || event.key === 'Control' || event.key === 'Alt') {
+    // Nothing to do
+  }
+  else {
+    if (spanElem.innerText > 0) {
+      spanElem.innerText -= 1
+    }
+    else {
+      inputElem.removeEventListener('keydown', inpHandler)
+    }
+  }
+}
